34e2819ca1d93aff6600ed40603e828100f55d16
x6eull <x6eull@gmail.com>
2025-07-22 05:16:34 +0800

    add SIMD Points-to bit-vector impl (WIP)

---
+++ svf/include/Util/SIMDHelper.h
@@ -0,0 +1,119 @@
+/**
+    SIMD helper functions for various bit lengths.
+    Use avx512 if available & applicable, otherwise fall back to avx2(256
+    bits)/sse4(128 bits)/uint64(64 bits).
+*/
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <immintrin.h>
+#include <popcntintrin.h>
+
+#define sse4_enabled 1
+#define avx256_enabled 1
+#define avx512_enabled 1
+
+#define ENSURE_LENGTH(BitLength)                                               \
+    static_assert(BitLength == 64 || BitLength == 128 || BitLength == 256 ||   \
+                      BitLength == 512,                                        \
+                  "Unsupported bit length");
+
+/// Returns true if all bits are zero.
+template <unsigned short BitLength> inline bool testz(const void* addr) {
+    ENSURE_LENGTH(BitLength);
+    if constexpr (BitLength == 512) {
+        // AVX512
+#if avx512_enabled
+        __m512i v = _mm512_loadu_si512(addr);
+        return _mm512_test_epi64_mask(v, v) == 0;
+#else
+        return testz<256>(addr) &&
+               testz<256>(reinterpret_cast<const std::byte*>(addr) + 32);
+#endif
+    } else if constexpr (BitLength == 256) {
+        // AVX2(avx256)
+#if avx256_enabled
+        __m256i v = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(addr));
+        return _mm256_testz_si256(v, v);
+#else
+        return testz<128>(addr) &&
+               testz<128>(reinterpret_cast<const std::byte*>(addr) + 16);
+#endif
+    } else if constexpr (BitLength == 128) {
+        // SSE4
+#if sse4_enabled
+        __m128i v = _mm_loadu_si128(reinterpret_cast<const __m128i*>(addr));
+        return _mm_testz_si128(v, v);
+#else
+        return testz<64>(addr) &&
+               testz<64>(reinterpret_cast<const std::byte*>(addr) + 8);
+#endif
+    } else // 64bit
+        return *reinterpret_cast<const uint64_t*>(addr) == 0;
+}
+
+/// Count bits set(logical 1) in a contiguous memory region.
+template <unsigned short BitLength>
+inline uint32_t popcnt(const void* addr, size_t size) {
+    ENSURE_LENGTH(BitLength);
+    if (size == 0) return 0;
+
+    /// only avx512 supports popcnt
+    if constexpr (BitLength == 512 && avx512_enabled) {
+        __m512i v0 = _mm512_loadu_si512(addr);
+        __m512i c = _mm512_popcnt_epi64(v0);
+        const __m512i* cur_addr = reinterpret_cast<const __m512i*>(addr);
+        for (int i = 1; i < size; i++) {
+            cur_addr++;
+            __m512i curv = _mm512_loadu_si512(cur_addr);
+            __m512i curc = _mm512_popcnt_epi64(curv);
+            c = _mm512_add_epi64(c, curc);
+        }
+        return _mm512_reduce_add_epi64(c);
+    }
+
+    const uint64_t* addr64 = reinterpret_cast<const uint64_t*>(addr);
+    const auto len = size * (BitLength / 64);
+    uint32_t result = 0;
+    for (size_t i = 0; i < len; i++, addr64++)
+        result += _mm_popcnt_u64(*addr64);
+    return result;
+}
+
+/// Returns true if all bits in v2 are set in v1. (that is, v1 contains v2)
+template <unsigned short BitLength>
+inline bool contains(const void* addr1, const void* addr2) {
+    ENSURE_LENGTH(BitLength);
+    static_assert(BitLength == 512 && avx512_enabled,
+                  "FIXME: Implement for other bit length");
+    __m512i v1 = _mm512_loadu_si512(addr1);
+    __m512i v2 = _mm512_loadu_si512(addr2);
+    __m512i r = _mm512_or_si512(v1, v2);
+    __mmask8 cmp = _mm512_cmpeq_epi64_mask(r, v2);
+    return cmp == 0xFF; // all bits in v2 are set in v1
+}
+
+/// Returns true if v1 and v2 share any bits.
+template <unsigned short BitLength>
+inline bool intersects(const void* addr1, const void* addr2) {
+    ENSURE_LENGTH(BitLength);
+    static_assert(BitLength == 512 && avx512_enabled,
+                  "FIXME: Implement for other bit length");
+    __m512i v1 = _mm512_loadu_si512(addr1);
+    __m512i v2 = _mm512_loadu_si512(addr2);
+    __m512i r = _mm512_and_si512(v1, v2);
+    // the result is non-zero if they share any bits
+    return _mm512_test_epi64_mask(r, r) != 0;
+}
+
+template <unsigned short BitLength>
+inline bool cmpeq(const void* addr1, const void* addr2) {
+    ENSURE_LENGTH(BitLength);
+    static_assert(BitLength == 512 && avx512_enabled,
+                  "FIXME: Implement for other bit length");
+    __m512i v1 = _mm512_loadu_si512(addr1);
+    __m512i v2 = _mm512_loadu_si512(addr2);
+    return _mm512_cmpeq_epi64_mask(v1, v2) == 0xFF;
+}
---
+++ svf/include/Util/SegmentBitVector.h
@@ -0,0 +1,311 @@
+#pragma once
+
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <iterator>
+#include <vector>
+
+#include "SIMDHelper.h"
+
+namespace SVF {
+using std::vector;
+class SegmentBitVector {
+    friend class SVFIRWriter;
+    friend class SVFIRReader;
+
+public:
+    static constexpr size_t SegmentBits = 512;
+    static constexpr size_t SegmentSize = SegmentBits / 8;
+
+    using UnitType = unsigned long long;
+    static constexpr size_t UnitSize = sizeof(UnitType);
+    static constexpr size_t UnitBits = UnitSize * 8;
+    static constexpr size_t UnitsPerSegment = SegmentSize / UnitSize;
+
+    /// A structure to hold 512 bits
+    struct Segment {
+        UnitType data[UnitsPerSegment];
+
+        /// Returns true if all bits are zero.
+        bool empty() const noexcept {
+            return testz<SegmentBits>(data);
+        }
+
+        bool test(size_t index) const noexcept {
+            const size_t unit_index = index / UnitBits;
+            const size_t bit_index = index % UnitBits;
+            return data[unit_index] & (1u << bit_index);
+        }
+
+        void set(size_t index) noexcept {
+            const size_t unit_index = index / UnitBits;
+            const size_t bit_index = index % UnitBits;
+            data[unit_index] |= (1u << bit_index);
+        }
+
+        void reset(size_t index) noexcept {
+            const size_t unit_index = index / UnitBits;
+            const size_t bit_index = index % UnitBits;
+            data[unit_index] &= ~(1u << bit_index);
+        }
+
+        bool contains(const Segment& rhs) const noexcept {
+            return ::contains<SegmentBits>(data, rhs.data);
+        }
+
+        bool intersects(const Segment& rhs) const noexcept {
+            return ::intersects<SegmentBits>(data, rhs.data);
+        }
+
+        bool operator==(const Segment& rhs) const noexcept {
+            return cmpeq<SegmentBits>(data, rhs.data);
+        }
+    } __attribute__((packed));
+
+    using indice_t = size_t;
+
+protected:
+    vector<indice_t> indices;
+    vector<Segment> data;
+
+public:
+    class SegmentBitVectorIterator {
+    public:
+        using iterator_category = std::input_iterator_tag;
+        using value_type = indice_t;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const indice_t*;
+        using reference = const indice_t&;
+        indice_t cur_pos;
+
+    protected:
+        typename vector<indice_t>::const_iterator indicesIt;
+        typename vector<indice_t>::const_iterator indicesEnd;
+        typename vector<Segment>::const_iterator dataIt;
+        unsigned char unit_index; // unit index in the current segment
+        unsigned char bit_index;  // bit index in the current unit
+        bool end;
+        /// move to next unit, returns false if reached the end
+        void incr_unit() {
+            bit_index = 0;
+            if (++unit_index == UnitsPerSegment) {
+                // forward to next segment
+                unit_index = 0;
+                ++indicesIt, ++dataIt;
+                if (indicesIt == indicesEnd) {
+                    // reached the end
+                    end = true;
+                }
+            }
+        }
+        /// Increment the bit index (mark the current bit as visited).
+        void incr_bit() {
+            if (++bit_index == UnitBits) {
+                // forward to next unit
+                incr_unit();
+            }
+        }
+        /// Start from current position and search for the next set bit
+        void search() {
+            while (!end) {
+                auto mask = ~((static_cast<UnitType>(1) << bit_index) - 1);
+                auto masked_unit = dataIt->data[unit_index] & mask;
+                auto tz_count = __tzcnt_u64(masked_unit);
+                if (tz_count < UnitBits) {
+                    // found a set bit
+                    bit_index = tz_count;
+                    cur_pos = *indicesIt + unit_index * UnitBits + bit_index;
+                    return;
+                } else // move to next unit
+                    incr_unit();
+            }
+        }
+        /// Step out from the current position and search for the next set bit.
+        void forward() {
+            incr_bit();
+            search();
+        }
+
+    public:
+        SegmentBitVectorIterator() = delete;
+        SegmentBitVectorIterator(const SegmentBitVector& vec, bool end = false)
+            : // must be init to identify raw vector
+              indicesEnd(vec.indices.end()), end(end) {
+            if (end) return;
+            indicesIt = vec.indices.begin();
+            dataIt = vec.data.begin();
+            unit_index = 0;
+            bit_index = 0;
+            search();
+        }
+        SegmentBitVectorIterator(const SegmentBitVectorIterator& other) =
+            default;
+        SegmentBitVectorIterator& operator=(
+            const SegmentBitVectorIterator& other) = default;
+
+        reference operator*() const {
+            return cur_pos;
+        }
+        pointer operator->() const {
+            return &cur_pos;
+        }
+        SegmentBitVectorIterator& operator++() {
+            forward();
+            return *this;
+        }
+        SegmentBitVectorIterator operator++(int) {
+            SegmentBitVectorIterator temp = *this;
+            ++*this;
+            return temp;
+        }
+        bool operator==(const SegmentBitVectorIterator& other) const {
+            return
+                // created from the same SegmentBitVector, and
+                indicesEnd == other.indicesEnd &&
+                (( // both ended, or
+                     end && other.end) ||
+                 ( // both not ended and pointing to the same position
+                     end == other.end && cur_pos == other.cur_pos));
+        }
+        bool operator!=(const SegmentBitVectorIterator& other) const {
+            return !(*this == other);
+        }
+    };
+
+    /// Returns an iterator to the beginning of this SegmentBitVector.
+    /// NOTE: If you modify the vector after creating an iterator, the iterator
+    /// is not stable and may cause UB if used.
+    const SegmentBitVectorIterator begin() const {
+        return SegmentBitVectorIterator(*this);
+    }
+    const SegmentBitVectorIterator end() const {
+        return SegmentBitVectorIterator(*this, true);
+    }
+
+    /// Construct empty SegmentBitVector
+    SegmentBitVector(void) {}
+
+    /// Copy constructor
+    SegmentBitVector(const SegmentBitVector& other)
+        : indices(other.indices), data(other.data) {}
+
+    /// Move constructor
+    SegmentBitVector(SegmentBitVector&& other) noexcept
+        : indices(std::move(other.indices)), data(std::move(other.data)) {}
+
+    /// Copy assignment
+    SegmentBitVector& operator=(const SegmentBitVector& other) {
+        if (this != &other) {
+            indices = other.indices;
+            data = other.data;
+        }
+        return *this;
+    }
+
+    /// Move assignment
+    SegmentBitVector& operator=(SegmentBitVector&& other) noexcept {
+        if (this != &other) {
+            indices = std::move(other.indices);
+            data = std::move(other.data);
+        }
+        return *this;
+    }
+
+    /// Returns true if no bits are set.
+    bool empty() const noexcept {
+        return indices.empty();
+    }
+
+    /// Returns the count of set bits.
+    uint32_t count() const noexcept {
+        return popcnt<SegmentBits>(data.data(), data.size());
+    }
+
+    /// Empty the set.
+    void clear() noexcept {
+        indices.clear();
+        data.clear();
+    }
+
+    /// Returns true if n is in this set.
+    bool test(uint32_t n) const noexcept {
+        for (int i = 0; i < indices.size(); ++i) {
+            const auto ind = indices[i];
+            if (ind > n) break; // early return
+            if (n >= ind && n < ind + SegmentBits)
+                return data[i].test(n % SegmentBits);
+        }
+        return false;
+    }
+
+    void set(uint32_t n) noexcept {
+        int i = 0;
+        for (; i < indices.size(); ++i) {
+            const auto ind = indices[i];
+            if (ind > n) break; // early return
+            if (n >= ind && n < ind + SegmentBits)
+                return data[i].set(n % SegmentBits);
+        }
+        indices.emplace(indices.begin() + i, n - (n % SegmentBits));
+        data.emplace(data.begin() + i);
+        return data[i].set(n % SegmentBits);
+    }
+
+    bool test_and_set(uint32_t n) noexcept {
+        // TODO: improve
+        const auto result = test(n);
+        set(n);
+        return result;
+    }
+
+    void reset(uint32_t n) noexcept {
+        for (int i = 0; i < indices.size(); ++i) {
+            const auto ind = indices[i];
+            if (ind > n) break; // early return
+            if (n >= ind && n < ind + SegmentBits) {
+                data[i].reset(n % SegmentBits);
+                if (data[i].empty()) { // this segment is empty, remove it
+                    indices.erase(indices.begin() + i);
+                    data.erase(data.begin() + i);
+                }
+                return;
+            }
+        }
+    }
+
+    /// Returns true if this set contains all bits of rhs.
+    bool contains(const SegmentBitVector& rhs) const noexcept {
+        size_t this_i = 0, rhs_i = 0;
+        while (this_i < indices.size() && rhs_i < rhs.indices.size()) {
+            const auto this_ind = indices[this_i];
+            const auto rhs_ind = rhs.indices[rhs_i];
+            if (this_ind > rhs_ind) return false;
+            if (this_ind < rhs_ind) ++this_i;
+            else {
+                if (!data[this_i].contains(rhs.data[rhs_i])) return false;
+                ++this_i;
+                ++rhs_i;
+            }
+        }
+        return rhs_i == rhs.indices.size();
+    }
+
+    /// Returns true if this set and rhs share any bits.
+    bool intersects(const SegmentBitVector& rhs) const noexcept {
+        size_t this_i = 0, rhs_i = 0;
+        while (this_i < indices.size() && rhs_i < rhs.indices.size()) {
+            const auto this_ind = indices[this_i];
+            const auto rhs_ind = rhs.indices[rhs_i];
+            if (this_ind > rhs_ind) ++rhs_i;
+            else if (this_ind < rhs_ind) ++this_i;
+            else {
+                if (this->data[this_i].intersects(rhs.data[rhs_i])) return true;
+                ++this_i;
+                ++rhs_i;
+            }
+        }
+        return false;
+    }
+};
+} // namespace SVF
